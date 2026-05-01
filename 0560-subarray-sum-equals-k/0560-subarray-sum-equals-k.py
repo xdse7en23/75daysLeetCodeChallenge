789bc68a0499cc733c2c598d5bc0cd2e55f1ea3f
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-05-01 11:44:48 +0530

    Time: 30 ms (83.03%), Space: 14.8 MB (46.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,17 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        prefix_sums = {0: 1}
+        prefix_map = {0: 1}
         current_sum = 0
         count = 0
+        
+        get_count = prefix_map.get
         
         for num in nums:
             current_sum += num
             
             diff = current_sum - k
-            if diff in prefix_sums:
-                count += prefix_sums[diff]
-            
-            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
+            if diff in prefix_map:
+                count += prefix_map[diff]
+                
+            prefix_map[current_sum] = get_count(current_sum, 0) + 1
             
         return count
