e861852852f1c5b444cb7706176cc45f5da684c1
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-05-12 19:41:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (98.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -7,23 +7,25 @@ public:
         int n1 = s1.length(), n2 = s2.length();
         if (n1 > n2) return false;
 
-        int count[26] = {0};
-        for (char c : s1) count[c - 'a']++;
+        int freq[26] = {0};
+        for (char c : s1) freq[c - 'a']++;
 
-        int left = 0, right = 0, required = n1;
+        int left = 0, right = 0, count = n1;
 
         while (right < n2) {
-            if (count[s2[right] - 'a'] > 0) {
-                required--;
+            if (freq[s2[right] - 'a'] >= 1) {
+                count--;
             }
-            count[s2[right] - 'a']--;
+            freq[s2[right] - 'a']--;
             right++;
-            if (required == 0) return true;
+
+            if (count == 0) return true;
+
             if (right - left == n1) {
-                if (count[s2[left] - 'a'] >= 0) {
-                    required++;
+                if (freq[s2[left] - 'a'] >= 0) {
+                    count++;
                 }
-                count[s2[left] - 'a']++;
+                freq[s2[left] - 'a']++;
                 left++;
             }
         }
