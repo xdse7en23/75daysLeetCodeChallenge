f1df7ac48e8f42af26857e3e543fe4656b4be6cd
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-05-28 21:24:11 +0530

    Time: 209 ms (93.59%), Space: 27.5 MB (95.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def numIslands(self, grid):
-        if not grid:
+        if not grid or not grid[0]:
             return 0
             
         ROWS = len(grid)
@@ -11,20 +11,26 @@ class Solution(object):
             for c in range(COLS):
                 if grid[r][c] == '1':
                     islands += 1
-                    stack = [(r, c)]
+                    grid[r][c] = '0'
+                    stack = [r * COLS + c]
                     
                     while stack:
-                        curr_r, curr_c = stack.pop()
-                        if grid[curr_r][curr_c] == '1':
-                            grid[curr_r][curr_c] = '0'
-                            
-                            if curr_r > 0 and grid[curr_r - 1][curr_c] == '1': 
-                                stack.append((curr_r - 1, curr_c))
-                            if curr_r < ROWS - 1 and grid[curr_r + 1][curr_c] == '1': 
-                                stack.append((curr_r + 1, curr_c))
-                            if curr_c > 0 and grid[curr_r][curr_c - 1] == '1': 
-                                stack.append((curr_r, curr_c - 1))
-                            if curr_c < COLS - 1 and grid[curr_r][curr_c + 1] == '1': 
-                                stack.append((curr_r, curr_c + 1))
+                        pos = stack.pop()
+                        curr_r = pos // COLS
+                        curr_c = pos % COLS
+                        
+                        if curr_r > 0 and grid[curr_r - 1][curr_c] == '1':
+                            grid[curr_r - 1][curr_c] = '0'
+                            stack.append((curr_r - 1) * COLS + curr_c)
+                        if curr_r < ROWS - 1 and grid[curr_r + 1][curr_c] == '1':
+                            grid[curr_r + 1][curr_c] = '0'
+                            stack.append((curr_r + 1) * COLS + curr_c)
+                        if curr_c > 0 and grid[curr_r][curr_c - 1] == '1':
+                            grid[curr_r][curr_c - 1] = '0'
+                            stack.append(curr_r * COLS + (curr_c - 1))
+                        if curr_c < COLS - 1 and grid[curr_r][curr_c + 1] == '1':
+                            grid[curr_r][curr_c + 1] = '0'
+                            stack.append(curr_r * COLS + (curr_c + 1))
                                 
         return islands
+
