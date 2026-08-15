ba67d4776cb5ce566d1cf03885cad4a1cc09f642
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-08-15 22:35:08 +0530

    Time: 3 ms (94.25%), Space: 12.3 MB (55.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,11 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 or (x % 10 == 0 and x != 0):
+        if x < 0:
             return False
             
-        reversed_half = 0
-        while x > reversed_half:
-            reversed_half = (reversed_half * 10) + (x % 10)
-            x //= 10
-        return x == reversed_half or x == reversed_half // 10
+        temp = x
+        reversed_num = 0
+        
+        while temp > 0:
+            reversed_num = (reversed_num * 10) + (temp % 10)
+            temp //= 10
+            
+        return x == reversed_num
+
