1800c6d88cbc91566cae5cca1cdb28ccb5e7a3bd
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-08-24 18:28:43 +0530

    Time: 486 ms (10.68%), Space: 249 MB (8.99%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 static const int fast_io = []() {
@@ -11,9 +11,11 @@ static const int fast_io = []() {
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0; 
-        int count = 1; 
-        vector<bool> isPrime(n, true);
+        if (n <= 2) return 0;
+        bool* isPrime = new bool[n];
+        memset(isPrime, true, n);
+
+        int count = 1;
         for (int i = 3; i * i < n; i += 2) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += 2 * i) {
@@ -26,6 +28,7 @@ public:
                 count++;
             }
         }
+        delete[] isPrime;
 
         return count;
     }
