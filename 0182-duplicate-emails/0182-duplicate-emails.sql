61fa652b40eae1096cf47d418c0061d643a22c01
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-08-24 18:21:11 +0530

    Time: 419 ms (47.43%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,7 @@
-SELECT email AS Email
-FROM (
-    SELECT email, COUNT(email) AS num
-    FROM Person
-    GROUP BY email
-) AS email_counts
-WHERE num > 1;
+SELECT DISTINCT p1.email AS Email
+FROM Person p1
+WHERE EXISTS (
+    SELECT 1 
+    FROM Person p2 
+    WHERE p1.email = p2.email AND p1.id < p2.id
+);
