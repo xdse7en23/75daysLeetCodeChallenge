27f85da7fea654eef840af74721c4d4dc3a84183
Shobhit Prajapati <117559866+xdse7en23@users.noreply.github.com>
2026-09-16 17:08:26 +0530

    Time: 0 ms (100%), Space: 68.3 MB (47.48%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <numeric>
 #include <algorithm>
 using namespace std;
 
@@ -7,21 +6,29 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n, 1);
+        if (n <= 1) return n;
+        
+        int totalCandies = 1;
+        int up = 0, down = 0, peak = 0;
         
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+                down = 0;
+                up++;
+                peak = up;
+                totalCandies += 1 + up;
+            } else if (ratings[i] == ratings[i - 1]) {
+                up = 0;
+                down = 0;
+                peak = 0;
+                totalCandies += 1;
+            } else {
+                up = 0;
+                down++;
+                totalCandies += 1 + down - (peak >= down ? 1 : 0);
             }
         }
         
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = max(candies[i], candies[i + 1] + 1);
-            }
-        }
-        
-        return accumulate(candies.begin(), candies.end(), 0);
+        return totalCandies;
     }
 };
-
